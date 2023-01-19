8ffecd484c7a7d38e9e7eac62a0962d7f384ebfa
esraamostfa <esraamostfa13@gmail.com>
2023-01-19 16:48:45 +0200

    Add basic login UI

--- ShoeStore/app/src/main/java/com/esraa/egfwd/shoestore/screens/login/LoginFragment.kt
+++ ShoeStore/app/src/main/java/com/esraa/egfwd/shoestore/screens/login/LoginFragment.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.esraa.egfwd.shoestore.R
+import com.esraa.egfwd.shoestore.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+
+        // Inflate view and obtain an instance of the binding class.
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        return  binding.root
     }
 }
